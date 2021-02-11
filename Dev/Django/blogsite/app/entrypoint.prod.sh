ab1e75a84f1cb7479e8cbfcf6c71d9b2d70fedfb
yuto <60546254+nosscafe66@users.noreply.github.com>
2021-02-11 20:56:27 +0900

    Add files via upload

---
+++ Dev/Django/blogsite/app/entrypoint.prod.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
