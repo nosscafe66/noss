5812401faf11b3cc5d1a32a29bb2487188777879
noss <nosscafe66@gmail.com>
2021-02-18 20:57:59 +0900

    Chat

---
+++ Chat/mychat/chat/consumers.py
@@ -0,0 +1,9 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+    
+    def disconnect(self,disconnect):
+      pass
+    
---
+++ Chat/mychat/mychat/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for mychat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+
+#application = get_asgi_application() #default
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter
+
+application = ProtocolTypeRouter( {
+    'http': django_asgi_app,
+} )
