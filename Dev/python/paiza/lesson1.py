04549f1b5c2052eb0be5fbdaf319788f79870dad
yuto <60546254+nosscafe66@users.noreply.github.com>
2021-04-03 17:17:46 +0900

    Create lesson1.py

---
+++ Dev/python/paiza/lesson1.py
@@ -0,0 +1,20 @@
+#https://paiza.jp/works/mondai/string_primer/basic_step7
+
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+N = int(input())
+for i in range(N):
+    j = input()
+    #j += j
+    print(j,end = "")
+    
+'''解答例
+n = int(input())
+
+ans = ""
+for _ in range(n):
+    ans += input()
+
+print(ans)
+'''
