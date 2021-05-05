7310b3f0c53e8cbe78c029b679d8ae7c157110b0
yuto <60546254+nosscafe66@users.noreply.github.com>
2021-05-05 22:35:03 +0900

    Create lesson31.py

---
+++ Dev/python/paiza/lesson31.py
@@ -0,0 +1,23 @@
+N = int(input())
+S_0,S_1,S_2,S_3,S_4,S_5,S_6,S_7,S_8,S_9 = input().split()
+
+if N == 1:
+    print(S_0)
+elif N == 2:
+    print(S_1)
+elif N == 3:
+    print(S_2)
+elif N == 4:
+    print(S_3)
+elif N == 5:
+    print(S_4)
+elif N == 6:
+    print(S_5)
+elif N == 7:
+    print(S_6)
+elif N == 8:
+    print(S_7)
+elif N == 9:
+    print(S_8)
+elif N == 10:
+    print(S_9)
