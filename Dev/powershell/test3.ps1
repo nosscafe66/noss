f9438d27fee5417406bb819d735053ac173b3955
yuto <60546254+nosscafe66@users.noreply.github.com>
2021-06-06 00:57:54 +0900

    Update test3.ps1

--- Dev/powershell/test3.ps1
+++ Dev/powershell/test3.ps1
@@ -1,6 +1,5 @@
-#Waiting状態のクラスタIDを取得する処理
+
 Function Cluster_Check($Cluster_Name){
-    #$Active_Cluster_Id = (aws emr list-clusters --active --query  "Clusters[?Name=='$Cluster_Name'].[Id]"--output text)
     while ($true){
         $Waiting_Status_Cluster = (aws emr list-clusters --cluster-states WAITING --query  "Clusters[?Name=='$Cluster_Name'].[Status.State]"--output text)
         Write-Host $Waiting_Status_Cluster
@@ -15,23 +14,22 @@ Function Cluster_Check($Cluster_Name){
     $Waiting_Cluster_Id = (aws emr list-clusters --cluster-states WAITING --query  "Clusters[?Name=='$Cluster_Name'].[Id]"--output text)
     return $Waiting_Cluster_Id
 }
-#Step実行処理
+
+
 Function Step_Run{
     Param(
         $Test_Cluster_Id,
         $Job_Name,
         $S3_Script_path
     )
-    #Write-Host "1：$Id 2：$Job_Name 3：$S3_Udf_path 4：$S3_Script_path 5：$Args1 6：$Args2 7：$Args3"
-    #$Step_Run_Id = (aws emr add-steps --cluster-id $Id --steps Type=spark,Name=$Job_Name,ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--py-files,$S3_Udf_path,$S3_Script_path,$Args1,$Args2,$Args3])
-   # {     "StepIds": [         "s-VYZUI500858M"     ] }
+
     $Step_Run = (aws emr add-steps --cluster-id $Test_Cluster_Id --steps Type=Spark,Name=$Job_Name,ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--py-files,$S3_Script_path])
     Write-Host $Step_Run
-    $Step_Run_Id = (aws emr list-steps --cluster-id $Test_Cluster_Id --query "Steps[?Name=='$Job_Name'].[Id]"--output text)#成功したステップ実行command
+    $Step_Run_Id = (aws emr list-steps --cluster-id $Test_Cluster_Id --query "Steps[?Name=='$Job_Name'].[Id]"--output text)
     Write-Host $Step_Run_Id
     return $Step_Run_Id
 }
-#ステップ実行のポーリング処理(10秒間隔)
+
 Function Polling_Run($Polling_Cluster_Id,$Polling_Step_Id){
     while ($true){
         $Step_Status = (aws emr describe-step --cluster-id $Polling_Cluster_Id --step-id $Polling_Step_Id --query "Step.Status.State"--output text)
@@ -51,7 +49,7 @@ Function Polling_Run($Polling_Cluster_Id,$Polling_Step_Id){
             }
      }         
 }
-#ステップIDが取得できているかの確認
+
 Function CheckNullOrEmpty($Cluster_Id,$Step_Id){
      if($null -eq $Step_Id){
         Write-Host "No_Step_Run"
@@ -66,11 +64,10 @@ Function CheckNullOrEmpty($Cluster_Id,$Step_Id){
 #メイン処理
 Function Main_Step_Start($NAME){
     $Cluster_Id = Cluster_Check $NAME
-    $Step_Id = Step_Run -Test_Cluster_Id $Cluster_Id -Job_Name job_test4 -S3_Script_path make_file.py 
-    #$Step_Id = "s-TLYDKQDSCUI"
+    $Step_Id = Step_Run -Test_Cluster_Id $Cluster_Id -Job_Name job_test4 -S3_Script_path make_file.py
     $Check = CheckNullOrEmpty $Cluster_Id $Step_Id
 }
-#戻り値の引き渡し
+
 Function Return_Args($return_args){
     $Return = Main_Step_Start $return_args
     Write-Host $Return
