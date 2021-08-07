27e368004883799e29e9b31b5f0b8c7d9b34c40a
yuto <60546254+nosscafe66@users.noreply.github.com>
2021-08-07 19:19:48 +0900

    Create coding_test_1.py

---
+++ Dev/python/etc/coding_test_1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def __init__(self):
+        self.nums = nums
+        self.target = target
+        
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        input()
+        for i in range(len(nums)):
+            print(nums[i] + nums[])
+            if nums == target:
+                nums[i]
+        print(type(nums))
+        print(target)
+        return
+
+        
+if __name__ == "__main__":
+    nums = input()
+    nums = eval(nums)
+    print(type(nums))
+    target = int(input())
+    calc = Solution()
+    calc.twoSum(nums,target)
+    
+        
