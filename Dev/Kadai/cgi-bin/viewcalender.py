e69bcf910f9a35d98844c1a9790e2f4dd8af4ba5
yuto <nosscafe66@gmail.com>
2022-03-14 22:58:33 +0900

    20commit

--- Dev/Kadai/cgi-bin/viewcalender.py
+++ Dev/Kadai/cgi-bin/viewcalender.py
@@ -4,8 +4,8 @@ alpdict_ = {'a':a,'m':m,'p':p,':':sep}
 all_dict = {**digdict_,**alpdict_}
 now = __import__('datetime').datetime.now()
 from time import strftime
-print('It may not work\n')
-print(f"Today is {now.year}-{now.month}-{now.day} \n")
+#print('It may not work\n')
+#print(f"Today is {now.year}-{now.month}-{now.day} \n")
 lh = ((int(strftime('%H'))-3+24))%24
 h,m= '00'+str(lh), '00'+strftime('%M')
 ftime = f"{int(h[-2])}{int(h[-1])}:{int(m[-2])}{int(m[-1])}"
@@ -15,17 +15,19 @@ full_time = f'{ftime}{meridiem[1].lower()}'
 x= 0
 y= 1
 z= 2
-for i in range(6):
-    for time in full_time:
-        for col in all_dict[time][i]:
-           if col == x:
-             print(' ', end = "")
-           elif col == y:
-             print('#', end = "")
-           elif col == z:
-             print('*', end='')
-        print(" ",end="")
-    print()
+#for i in range(6):
+#    for time in full_time:
+#        for col in all_dict[time][i]:
+#           if col == x:
+#             print(' ', end = "")
+#           elif col == y:
+#             print('#', end = "")
+#           elif col == z:
+#             print('*', end='')
+#        print(" ",end="")
+#    print()
 #print(det('st')) if int(now.month) == 1 or str(int(now.month))[-1] == '1' else print(det('nd')) if int(now.month) == 2 or str(now.month)[-1] == '2' else print(det('rd')) if int(now.month) == 3 or str(now.month)[-1] == '3' else print(det('th'))
 print()
 [print(__import__('calendar',fromlist=[None]).month(now.year,now.month,4))]
+APP=__import__('calendar',fromlist=[None]).calendar(now.year)
+print(APP)
