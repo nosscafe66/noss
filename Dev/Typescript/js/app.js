074719342534d7945c692d73a5bcf59fe76af228
yuto <60546254+nosscafe66@users.noreply.github.com>
2022-07-09 18:55:46 +0900

    codingtest3

--- Dev/Typescript/js/app.js
+++ Dev/Typescript/js/app.js
@@ -1,36 +1,75 @@
-const question = "住んでいるところはどれですか？";
-const answers = ['神奈川県','茨城県','東京都','京都'];
-const result = '神奈川県';
+const quiz = [
+    {
+    question: "エンジニア何年目ですか？",
+    answers:[
+        '1年目',
+        '2年目',
+        '3年目',
+        '4年目'
+    ],
+    result:'3年目'
+},{
+    question: "一番好きな業務・工程はなんですか？",
+    answers:[
+        'コーディング',
+        '設計',
+        '要件',
+        '運用'
+    ],
+    result:'コーディング'
+},{
+    question: "開発言語で好きなものはどれですか？",
+    answers:[
+        'Python',
+        'TypeScript',
+        'PHP',
+        'Ruby'
+    ],
+    result:'Python'
+}
+]
+
+const quizlength = quiz.length;
+let quizIndex = 0;
+
 const $button = document.getElementsByTagName('button');
+let buttonLength = $button.length;
 
 const setQuiz = () => {
-    document.getElementById('question').textContent = question;
+    document.getElementById('question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
-    let buttonLength = $button.length;
     while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex ++;
     };
 }
 setQuiz();
 
 const clickHandler = (e) => {
-    if(result === e.target.textContent){
+    if(quiz[quizIndex].result === e.target.textContent){
         window.alert('正解！');
     }else{
         window.alert('不正解');
     }
+    quizIndex ++;
+
+    if(quizIndex < quizlength){
+        setQuiz();
+    }else{
+        window.alert("終了");
+    }
 }
 
-$button[0].addEventListener('click', (e) =>{
-    clickHandler(e);
-});
-$button[1].addEventListener('click', (e) =>{
-    clickHandler(e);
-});
-$button[2].addEventListener('click', (e) =>{
-    clickHandler(e);
-});
-$button[3].addEventListener('click', (e) =>{
-    clickHandler(e);
-});
+const handleIndex = () => {
+    let handleIndex = 0;
+    while(handleIndex < buttonLength){
+        $button[handleIndex].addEventListener('click', (e) =>{
+            clickHandler(e);
+        });
+        handleIndex ++;
+    };
+}
+handleIndex();
+
+const jsonData= require('./json/info.json'); 
+console.log(jsonData);
