f77f3b636fcab066fff7d83b3d682c8e45fa3ea0
yuto <60546254+nosscafe66@users.noreply.github.com>
2022-07-30 09:17:34 +0900

    commit1

---
+++ Dev/Typescript/venv/lib/python3.10/site-packages/pywinctl/test.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def _levenshtein(seq1: str, seq2: str, similarity: int = 90) -> bool:
+    # https://stackabuse.com/levenshtein-distance-and-text-similarity-in-python/
+    # Adapted to return a similarity percentage, which is easier to define
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x - 1] == seq2[y - 1]:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1],
+                    matrix[x, y - 1] + 1
+                )
+            else:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1] + 1,
+                    matrix[x, y - 1] + 1
+                )
+    dist = matrix[size_x - 1, size_y - 1]
+    return ((1 - dist / max(len(seq1), len(seq2))) * 100) >= similarity
+
+
+print(_levenshtein("La reunión no está en curso... - Google Chromic", "La reunión está en curso... - Google Chrome"))
